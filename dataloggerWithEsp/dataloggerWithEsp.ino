6941d0b930d4a696cb455a5d6d18bc8fc8708f48
Henrique Fleck Harff <henriquefleckh04@gmail.com>
2023-01-13 18:20:03 -0300

    Fix the baudrate to equal of the sensor

--- dataloggerWithEsp/dataloggerWithEsp.ino
+++ dataloggerWithEsp/dataloggerWithEsp.ino
@@ -1,5 +1,5 @@
 void setup() {
-  Serial.begin(9600); // opens serial port, sets data rate to 9600 bps
+  Serial.begin(115200); // opens serial port, sets data rate to 9600 bps
 }
 
 void loop() {
@@ -7,9 +7,6 @@ void loop() {
   if (Serial.available() > 0) {
     // read the incoming string:
     String incomingString = Serial.readString();
-
-    // prints the received data
-    Serial.print("I received: ");
     Serial.println(incomingString);
   }
 }
