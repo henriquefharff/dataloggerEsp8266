8cd1b809f8e636962571f09626a142ac6a8658ba
Henrique Fleck Harff <henriquefleckh04@gmail.com>
2023-01-13 09:48:58 -0300

    Working with flash of the esp8266
    This code save the last state about the led, and use a archive .txt into the flash for do this control

--- dataloggerWithEsp/dataloggerWithEsp.ino
+++ dataloggerWithEsp/dataloggerWithEsp.ino
@@ -1,17 +1,95 @@
-#include <Arduino.h>
+#include <FS.h>
+ 
+#define LED_PIN 2
 
-int data=0;
+ 
+/**
+  * @desc escreve conteúdo em um arquivo
+  * @param string state - conteúdo a se escrever no arquivo
+  * @param string path - arquivo a ser escrito
+*/
+void writeFile(String state, String path) { 
+  //Abre o arquivo para escrita ("w" write)
+  //Sobreescreve o conteúdo do arquivo
+  File rFile = SPIFFS.open(path,"w+"); 
+  if(!rFile){
+    Serial.println("Erro ao abrir arquivo!");
+  } else {
+    rFile.println(state);
+    Serial.print("gravou estado: ");
+    Serial.println(state);
+  }
+  rFile.close();
+}
+ 
+/**
+  * @desc lê conteúdo de um arquivo
+  * @param string path - arquivo a ser lido
+  * @return string - conteúdo lido do arquivo
+*/
+String readFile(String path) {
+  File rFile = SPIFFS.open(path,"r");
+  if (!rFile) {
+    Serial.println("Erro ao abrir arquivo!");
+  }
+  String content = rFile.readStringUntil('\r'); //desconsidera '\r\n'
+  Serial.print("leitura de estado: ");
+  Serial.println(content);
+  rFile.close();
+  return content;
+}
+ 
+/**
+  * @desc inicializa o sistema de arquivos
+*/
+void openFS(void){
+  //Abre o sistema de arquivos
+  if(!SPIFFS.begin()){
+    Serial.println("\nErro ao abrir o sistema de arquivos");
+  } else {
+    Serial.println("\nSistema de arquivos aberto com sucesso!");
+  }
+}
  
 void setup() {
- Serial.begin(9600);
+  pinMode(LED_PIN, OUTPUT);
+  Serial.begin(9600);   
+  openFS();
+   
+  // no primeiro upload de programa o arquivo state.txt deve ser criado com o conteúdo "off"
+  // no segundo upload a linha deve ser comentada.
+  //writeFile("off", "/state.txt");
+ 
+  // verifica o último estado do LED e ativa de acordo
+  String state = readFile("/state.txt");
+  if(state == "on") 
+  {
+    digitalWrite(LED_PIN, HIGH);
+  }
+  else if(state == "off") 
+  {
+    digitalWrite(LED_PIN, LOW);
+  }    
 }
  
 void loop() {
-  data = Serial.read();
-  if(data=='2')
+  /*
+   * verifica o estado anterior salvo no arquivo, 
+   * ativa o LED de acordo 
+   * e escreve novo estado no arquivo.
+   */
+  if(Serial.read() == '1')
   {
-    Serial.print("Boa meu bruxo\n");
-    data-=48;
-    Serial.println(data, DEC);
+    String state = readFile("/state.txt");    
+    if(state == "off") 
+    {
+      writeFile("on", "/state.txt");
+      digitalWrite(LED_PIN, LOW);
+    }
+    else if(state == "on") 
+    {
+      writeFile("off", "/state.txt");
+      digitalWrite(LED_PIN, HIGH);      
+    } 
   }
 }
